5c8f6c10a8ae54235cf360097242d4b355655e66
Ulmanu <ulmanu.cristi98@gmail.com>
2020-08-11 13:35:04 +0300

    Aparitia elem dupa 3 sec

--- dist/script.js
+++ dist/script.js
@@ -978,9 +978,26 @@ function () {
   _createClass(Slider, [{
     key: "showSlides",
     value: function showSlides(n) {
+      var _this = this;
+
       if (n > this.slides.length) {
         this.slideIndex = 1;
       }
+
+      try {
+        this.hanson.style.opacity = '0';
+
+        if (n === 3) {
+          this.hanson.classList.add('animated');
+          setTimeout(function () {
+            _this.hanson.style.opacity = '1';
+
+            _this.hanson.classList.add('slideInUp');
+          }, 3000);
+        } else {
+          this.hanson.classList.remove('slideInUp');
+        }
+      } catch (e) {}
 
       if (n < 1) {
         this.slideIndex = this.slides.length;
@@ -1001,17 +1018,21 @@ function () {
   }, {
     key: "render",
     value: function render() {
-      var _this = this;
+      var _this2 = this;
+
+      try {
+        this.hanson = document.querySelector('.hanson');
+      } catch (e) {}
 
       this.btns.forEach(function (item) {
         item.addEventListener('click', function () {
-          _this.plusSlides(1);
+          _this2.plusSlides(1);
         });
         item.parentNode.previousElementSibling.addEventListener('click', function (e) {
           e.preventDefault();
-          _this.slideIndex = 1;
+          _this2.slideIndex = 1;
 
-          _this.showSlides(_this.slideIndex);
+          _this2.showSlides(_this2.slideIndex);
         });
       });
       this.showSlides(this.slideIndex);
--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -14,6 +14,24 @@ export default class Slider {
         if (n > this.slides.length) {
             this.slideIndex = 1;
         }
+        try
+        {
+            this.hanson.style.opacity='0';
+            if(n===3)
+            {
+                this.hanson.classList.add('animated');
+                setTimeout(()=>
+                {
+                    this.hanson.style.opacity='1';
+                    this.hanson.classList.add('slideInUp');
+                },3000);
+            }
+            else
+            {
+                this.hanson.classList.remove('slideInUp');
+            }
+        }catch(e){}
+      
         if (n < 1) {
             this.slideIndex = this.slides.length;
         }
@@ -33,6 +51,16 @@ export default class Slider {
     }
 
     render() {
+        try
+        {
+            this.hanson=document.querySelector('.hanson');
+        }
+        catch(e)
+        {
+
+        }
+        
+
       this.btns.forEach(item=>
         {
             item.addEventListener('click',()=>
